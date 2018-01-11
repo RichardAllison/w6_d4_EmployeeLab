5ddb61f106e0ba4aad5ea99db9d669f82cb52036
Richard Allison <richard.allison.18@outlook.com>
2018-01-11 15:54:28 +0000

    Dev and dbAdmin tests pass

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
 import techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
 
 public class DatabaseAdminTest {
 
@@ -7,7 +10,34 @@ public class DatabaseAdminTest {
 
     @Before
     public void before() {
-
         databaseAdmin = new DatabaseAdmin("Me", "999", 20000);
     }
+
+
+    @Test
+    public void hasName() {
+        assertEquals("Me", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNINO(){
+        assertEquals("999", databaseAdmin.getNi());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(20000, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        databaseAdmin.raiseSalary(7000);
+        assertEquals(27000, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(200, databaseAdmin.getBonus(), 0.01);
+    }
+
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
 import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
 
 public class DeveloperTest {
 
@@ -8,6 +11,32 @@ public class DeveloperTest {
     @Before
     public void before() {
         developer = new Developer("Me", "999", 20000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Me", developer.getName());
+    }
+
+    @Test
+    public void hasNINO(){
+        assertEquals("999", developer.getNi());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(20000, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        developer.raiseSalary(7000);
+        assertEquals(27000, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonus(){
+        assertEquals(200, developer.getBonus(), 0.01);
     }
 
 
